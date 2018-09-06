ef3baf344c22afd20db07dc16ffebb7e0e79f712
sminez <innesdmorrison@gmail.com>
2018-09-06 16:52:09 +0100

    adding threading utility functions

--- pa/utils.py
+++ pa/utils.py
@@ -2,6 +2,7 @@
 Utility functions and constants for pa
 '''
 import os
+import concurrent.futures
 from datetime import datetime
 
 import toml
@@ -117,3 +118,65 @@ def print_yellow(s, end='\n'):
 def print_green(s, end='\n'):
     '''Helper to give coloured output.'''
     print('{}{}{}'.format(GREEN, s, NC), end=end)
+
+
+def run_many(func, args_list, max_threads=10, fail_quiet=False):
+    '''
+    Run a function multiple times with different inputs,
+    each on its own thread. Intended for use with blocking IO
+    '''
+    results = []
+    max_workers = min([max_threads, len(args_list)])
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as ex:
+        futures = (
+            ex.submit(func, *args)
+            for args in args_list
+        )
+
+        for f in concurrent.futures.as_completed(futures):
+            try:
+                res = f.result()
+                if res:
+                    results.append(res)
+            except Exception as e:
+                if fail_quiet:
+                    pass
+                else:
+                    raise e
+
+    return results
+
+
+def run_many_tagged(func, tag_args, max_threads=10, fail_quiet=False):
+    '''
+    Run a function multiple times with different inputs,
+    each on its own thread. Intended for use with blocking IO
+    '''
+    def tagged(tag, func):
+        '''Helper to maintain tags'''
+        def _inner(*args):
+            return tag, func(*args)
+        return _inner
+
+    results = {}
+    max_workers = min([max_threads, len(tag_args)])
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as ex:
+        futures = (
+            ex.submit(tagged(tag, func), *args)
+            for (tag, args) in tag_args
+        )
+
+        for f in concurrent.futures.as_completed(futures):
+            try:
+                tag, res = f.result()
+                if res:
+                    results[tag] = res
+            except Exception as e:
+                if fail_quiet:
+                    pass
+                else:
+                    raise e
+
+    return results
